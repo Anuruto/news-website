7884540c4299ef94e4e5ce238321ff5ba99724b5
AnaKen <the.anaken@gmail.com>
2022-03-23 19:26:05 +0530

    done html

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log("Hello, World!");
 
 let apiKey = "16a449a5e7db4b66835eaca25891512c";
+let link =  `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`
+
+
 
 let newsDiv = document.getElementById("newsAccordion");
 
